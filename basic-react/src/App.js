4f216ffdda35638a2924f1a78a7717ad9f5d7d8c
Nazri Dwi Liansyah <nazri.dwiliansyah78@gmail.com>
2019-07-06 07:13:33 +0700

    practice-4

--- basic-react/src/App.js
+++ basic-react/src/App.js
@@ -2,8 +2,11 @@ import React , {Component} from 'react';
 import {Button} from 'reactstrap';
 
 import Alert from './components/organisms/Alert';
-import Drop from './components/organisms/Dropdown'
-import Grooup from './components/organisms/ButtonGrup2'
+import Drop from './components/organisms/Dropdown';
+import Grooup from './components/organisms/ButtonGrup2';
+import Form from './components/organisms/Form';
+import FormGrid from './components/organisms/FormGrid'
+import Modal from './components/organisms/Modal'
 class App extends Component {
   render(){
   return (
@@ -20,7 +23,9 @@ class App extends Component {
     </div>
     <Grooup/>
     </div>
-
+    <Form/>
+    <FormGrid/>
+    <Modal/>
   );
 }
 }
